d646946bef03970feb65fa2e9638f97fbb4ed912
Aleksej Kvasnikov <aleksejkvasnikov@gmail.com>
2016-11-08 20:29:07 +0700

    Changed

--- app/ES.m
+++ app/ES.m
@@ -1,34 +1,34 @@
-% начальные условия
+% init
 lamd = 10;
 u = 3;
 N = 40;
 t = 1/sqrt(N);
-g = 1; % нулевая генерация
+g = 1; %  0 generation
 gMax = 1000;
 
 q = ones(1, gMax); 
-q1 = ones(1, lamd); % вспомогательная
+q1 = ones(1, lamd); % for algorithm
 y = ones(gMax, gMax); 
-y1 = ones(lamd, gMax); %  вспомогательная
+y1 = ones(lamd, gMax); %  for algorithm
 
 F1 =  zeros(1,lamd);
 
-while g<gMax % пока не достигли gMax
+while g<gMax % 
     
    for l = 1 : lamd %
     q1(l)=q(g) * exp(randn * t); % 
-    z = transpose(mvnrnd(0, eye, gMax)); % генерируем вектор Z
+    z = transpose(mvnrnd(0, eye, gMax)); % gen Z vector
     x = q1(l)*z; % 
     y1(l,:)=y(g,:)+x; %
-        for iter = 1 : N % в цикле до 40 суммируем F l
-            somval = iter*y1(l,iter).^2; % вычисления
+        for iter = 1 : N % calculating F
+            somval = iter*y1(l,iter).^2; % 
             F1(l)=F1(l)+somval ; % 
         end
    end
-   [F1SORT,ind] = sort(F1, 'descend'); % сортируем вектор
-   q1Sort = q1(ind); % сортировка массива скаляров по полученным индексам
+   [F1SORT,ind] = sort(F1, 'descend'); % vector sorting
+   q1Sort = q1(ind); % sorting q1 with vector sorting indexes
    %q1Sort = sort(q1);
-   y1Sort = y1(ind,:); % сортировка матрицы по строкам(векторы) по полученным индексам
+   y1Sort = y1(ind,:); % sorting y1 with vector sorting indexes
    %y1Sort = sortrows(y1);
    q(g+1)=(1/u)*sum(q1Sort(1:u));
    y(g+1,:)=(1/u)*sum(y1Sort(1:u,:));
